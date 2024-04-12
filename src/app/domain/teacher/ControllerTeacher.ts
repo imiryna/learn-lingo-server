e35a2df625d630f28d2b4eb3aa44eedb91ec43bf
Iryna Burlaka <i.am.blinova@gmail.com>
2024-04-12 13:13:25 +0200

    some edditions to contoller

--- src/app/domain/teacher/ControllerTeacher.ts
+++ src/app/domain/teacher/ControllerTeacher.ts
@@ -1,6 +1,8 @@
 import { JsonController, Get, Post, Body, Param } from "routing-controllers";
 
 import { ITeacher } from "./Teacher.types";
+import { ApiResponse } from "helpers/ApiResponse";
+import { ApiError } from "helpers/ApiError";
 
 const storeData: ITeacher[] = [];
 
@@ -8,23 +10,28 @@ const storeData: ITeacher[] = [];
 export default class Teacher {
   @Get()
   async getAll() {
-    return storeData;
+    return new ApiResponse(true, storeData);
   }
 
   @Get("/:id")
-  async getOne(@Param("id") id: number): Promise<ITeacher | {}> {
+  async getOne(@Param("id") id: number): Promise<ApiResponse<ITeacher | {}>> {
     const teacher = storeData.find((item) => {
       return item.id === id;
     });
-
-    return teacher || {};
+    if (!teacher) {
+      throw new ApiError(404, {
+        code: "TEACHER_NOT_FOUND",
+        message: `Teacher with id ${id} not found`,
+      })
+    }
+    return new ApiResponse(true, teacher);
   }
 
   @Post()
   async setTeacher(@Body() body: ITeacher) {
     storeData.push(body);
 
-    return true;
+    return new ApiResponse(true, "Person successfully created");
   }
 }
 
