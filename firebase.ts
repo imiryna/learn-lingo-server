ba6a75b1640247e5ceef355bf7b2685a0c67a622
Iryna Burlaka <i.am.blinova@gmail.com>
2024-04-26 09:34:11 +0200

    firebase castom config

--- firebase.ts
+++ firebase.ts
@@ -1,14 +1,17 @@
-import config from "./config";
+// import config from "./config";
 import admin from "firebase-admin";
+import serviceAccount from "./serviceAccountKey.json";
 
-import * as fs from "fs";
-import * as path from "path";
+// import * as fs from "fs";
+// import * as path from "path";
 
-const serviceAccount = fs.readFile(path.join(__dirname, "../../client/index.html"), "utf8", (error, data) => {
-  // ...
-});
+// console.log(__dirname);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+// const serviceAccount = fs.readFile(path.join(__dirname, "serviceAccountKey.json"), "utf8", (error, data) => {
+//   console.log(data);
+// });
+
+export const firebase = admin.initializeApp({
+  credential: admin.credential.cert(JSON.stringify(serviceAccount)),
   databaseURL: "https://learn-lingo-f84ed-default-rtdb.europe-west1.firebasedatabase.app",
 });
